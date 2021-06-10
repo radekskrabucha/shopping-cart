1bdd147cc840cfda8e84c593f441a38321fe40a4
Radek Skrabucha <rskrabucha13@gmail.com>
2021-06-10 17:55:59 +0200

    fixed error page and github pages routing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Error from './pages/Error'
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path='/'>
           <Home />
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Layout from '../components/Layout'
 
 const Error = () => {
    return (
       <Layout>
-         Hello from error page!
+         <h2 className='title'>
+            Oopsss..we can't find the page you're looking for..
+         </h2>
+         <Link to='/' className='btn'>
+               Take me back home
+            </Link>
       </Layout>
    )
 }
