bf36e30f2feefae7ccb21a71f910c3a68ad7a590
Radek Skrabucha <rskrabucha13@gmail.com>
2021-06-10 18:17:38 +0200

    Style adjustments

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -39,7 +39,7 @@ const Cart = () => {
                      <h2 className='title'>
                         Your cart is empty
                      </h2>
-                     <svg class="cart-empty-svg" xmlns="http://www.w3.org/2000/svg" width="160" height="160" viewBox="0 0 24 24" fill="currentColor"><path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z"></path></svg>
+                     <svg className="cart-empty-svg" xmlns="http://www.w3.org/2000/svg" width="160" height="160" viewBox="0 0 24 24" fill="currentColor"><path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z"></path></svg>
                      <Link onClick={toggleCart} to='/products' className='btn'>
                         Browse products
                      </Link>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,34 +11,34 @@ const Footer = () => {
             <div className="footer-container">
                <ul className="policy-links">
                   <li>
-                     <a className='social-link' href="#">
+                     <a className='social-link' href="/">
                         REFUND POLICY
                      </a>
                   </li>
                   <li>
-                     <a className='social-link' href="#">
+                     <a className='social-link' href="/">
                         PRIVACY POLICY
                      </a>
                   </li>
                   <li>
-                     <a className='social-link' href="#">
+                     <a className='social-link' href="/">
                      SHIPPING POLICY
                      </a>
                   </li>
                </ul>
                <ul className="social-links">
                   <li>
-                     <a className='social-link' href="#">
+                     <a className='social-link' href="/">
                         <FaTwitter />
                      </a>
                   </li>
                   <li>
-                     <a className='social-link' href="#">
+                     <a className='social-link' href="/">
                      <FaInstagram />
                      </a>
                   </li>
                   <li>
-                     <a className='social-link' href="#">
+                     <a className='social-link' href="/">
                      <FaFacebook />
                      </a>
                   </li>
